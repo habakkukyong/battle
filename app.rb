9a6afa1545efdfda51aa8c921649b80cbcf9e5aa
habakkukyong <habakkukyong@gmail.com>
2019-01-22 17:40:16 +0000

    Refactor play page load into get request

--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  set :session_secret, 'super secret'
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/names'
+  end
+
+  get '/names' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    p @player_1_name
     erb(:play)
   end
 
