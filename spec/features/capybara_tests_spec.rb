1c3f93de18843bfb3a9108fa7004ec297cab945b
habakkukyong <habakkukyong@gmail.com>
2019-01-22 16:57:24 +0000

    Add form which takes player names

--- spec/features/capybara_tests_spec.rb
+++ spec/features/capybara_tests_spec.rb
@@ -7,9 +7,19 @@ require 'selenium-webdriver'
 #   visit 'http://localhost:2345'
 # end
 
-feature 'Testing infrastructure' do
-  scenario 'Can runa app and check page content' do
+# feature 'Testing infrastructure' do
+#   scenario 'Can runa app and check page content' do
+#     visit('/')
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+# end
+
+feature 'Testing filling in names' do
+  scenario 'Will set a name perameter to a name entered into a form' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player_1_name, with: 'Bob'
+    fill_in :player_2_name, with: 'Rob'
+    click_button('Submit')
+    expect(page).to have_content 'Bob vs. Rob'
   end
 end
