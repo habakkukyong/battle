6db5161d0f3f1f0781752e24fc7924e9d573ef91
habakkukyong <habakkukyong@gmail.com>
2019-01-23 16:20:18 +0000

    Pass test for confirmation that player 2 attacked

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -8,20 +8,28 @@ require 'selenium-webdriver'
 # end
 
 # feature 'Testing infrastructure' do
-#   scenario 'Can runa app and check page content' do
+#   scenario 'Can run app and check page content' do
 #     visit('/')
 #     expect(page).to have_content 'Testing infrastructure working!'
 #   end
 # end
 
-feature 'Testing play page' do
-  scenario 'Will set a name parameter to a name entered into a form' do
+feature 'Play page' do
+  scenario 'Displays player name params' do
     sign_in_and_play
     expect(page).to have_content 'p1 vs. p2'
   end
 
-  scenario 'when player names have been provided hitpoints shown' do
+  scenario 'Displays player 2 hitpoints' do
     sign_in_and_play
-    expect(page).to have_content 'p2 hitpoints: 100'
+    expect(page).to have_content 'p2 hitpoints: 60'
+  end
+end
+
+feature 'Attack page' do
+  scenario 'Confirms that player 2 has been attacked' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content 'p2 was attacked'
   end
 end
