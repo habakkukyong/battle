25a9d6874e096311342e50ca7f7b7bc1fd8ba23a
habakkukyong <habakkukyong@gmail.com>
2019-01-24 15:21:17 +0000

    Small refactor

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,13 @@
 require 'game'
 
 describe Game do
+  subject(:game) { described_class.new }
   let(:p2) { double :p2 }
 
   describe "#attack" do
     it 'calls receive damage on the argument' do
       expect(p2).to receive(:receive_damage)
-      subject.attack(p2)
+      game.attack(p2)
     end
   end
 end
